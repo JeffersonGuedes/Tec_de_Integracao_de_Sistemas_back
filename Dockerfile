74fccba0e50ded896fe5435c237a6e2f6333679f
Jefferson Guedes <jeffersonguedes0211@gmail.com>
2025-03-29 14:18:37 -0300

    :tada:

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3.11-slim
 
 RUN apt-get update && apt-get install -y \
     libpq-dev gcc \
+    dos2unix \
     && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
@@ -15,6 +16,9 @@ COPY start.sh /start.sh
 RUN dos2unix /start.sh && \
     chmod +x /start.sh
 
+ENV PYTHONPATH=/app \
+    PYTHONUNBUFFERED=1
+
 EXPOSE 8000
 
-CMD ["/start.sh"]
+CMD ["sh", "/start.sh"]
--- start.sh
+++ start.sh
@@ -1,8 +1,19 @@
+#!/bin/sh
+
+echo "🚀 Iniciando Gunicorn..."
 gunicorn --bind 0.0.0.0:8000 core.wsgi:application &
 
+echo "🔄 Iniciando Celery Worker..."
+celery -A core worker -l INFO &
+
+echo "⏰ Iniciando Celery Beat..."
 celery -A core beat -l INFO &
 
-python /consumers/generate_certificate.py &
-python /consumers/send_notification.py &
+echo "📨 Iniciando Consumer: generate_certificate.py..."
+python /app/consumers/generate_certificate.py &
 
-tail -f /dev/null
+echo "📩 Iniciando Consumer: send_notification.py..."
+python /app/consumers/send_notification.py &
+
+echo "✅ Todos os processos foram iniciados!"
+while :; do sleep 3600; done
