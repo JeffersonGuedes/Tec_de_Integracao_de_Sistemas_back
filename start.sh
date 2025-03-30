eaa99422875f2298d45c5ca7eeaeedca677f8d63
Jefferson Guedes <jeffersonguedes0211@gmail.com>
2025-03-29 21:47:25 -0300

    :tada:

--- start.sh
+++ start.sh
@@ -4,8 +4,10 @@ echo "🚀 Iniciando Gunicorn..."
 gunicorn --bind 0.0.0.0:8000 core.wsgi:application &
 
 echo "🔄 Iniciando Celery Worker..."
-celery -A core worker --uid= --gid= --loglevel=info
-celery -A core beat --uid= --gid=
+celery -A core worker --uid= --gid= --loglevel=info &
+
+echo "🔄 Iniciando Celery Beat..."
+celery -A core beat --uid= --gid= &
 
 echo "📨 Iniciando Consumer: generate_certificate.py..."
 python /app/consumers/generate_certificate.py &
